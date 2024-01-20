1ef23a2768b47d8b6610b0042b4788cfc5a4e32c
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 23:33:57 +0300

    bedroom is a room oldugundan aralarinda inherit bir iliski olusturuldu

--- src/com/workintech/model/Bedroom.java
+++ src/com/workintech/model/Bedroom.java
@@ -1,68 +1,18 @@
 package com.workintech.model;
 
-public class Bedroom {
+public class Bedroom extends Room{
     private String name;
-    private Wall wall1;
-    private Wall wall2;
-    private Wall wall3;
-    private Wall wall4;
-    private Ceiling ceiling;
     private Bed bed;
-    private Lamp lamp;
     private Wardrobe wardrobe;
-    private Carpet carpet;
 
-    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp, Wardrobe wardrobe, Carpet carpet) {
+
+    public Bedroom(Wall wall1, Wall wall2, Wall wall3, Wall wall4,
+                   Ceiling ceiling, Lamp lamp, String name, Carpet carpet,
+                   Bed bed, Wardrobe wardrobe) {
+        super(wall1, wall2, wall3, wall4, ceiling, lamp, carpet);
         this.name = name;
-        this.wall1 = wall1;
-        this.wall2 = wall2;
-        this.wall3 = wall3;
-        this.wall4 = wall4;
-        this.ceiling = ceiling;
         this.bed = bed;
-        this.lamp = lamp;
         this.wardrobe = wardrobe;
-        this.carpet = carpet;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Wall getWall1() {
-        return wall1;
-    }
-
-    public Wall getWall2() {
-        return wall2;
-    }
-
-    public Wall getWall3() {
-        return wall3;
-    }
-
-    public Wall getWall4() {
-        return wall4;
-    }
-
-    public Ceiling getCeiling() {
-        return ceiling;
-    }
-
-    public Bed getBed() {
-        return bed;
-    }
-
-    public Lamp getLamp() {
-        return lamp;
-    }
-
-    public Wardrobe getWardrobe() {
-        return wardrobe;
-    }
-
-    public Carpet getCarpet() {
-        return carpet;
     }
 }
 
