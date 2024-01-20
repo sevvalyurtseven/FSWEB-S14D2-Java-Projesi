f4f93dac68082fa0bcdef19284fe89eb8a81fab7
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 22:32:39 +0300

    getter methodları olusturuldu

--- src/com/workintech/model/Ceiling.java
+++ src/com/workintech/model/Ceiling.java
@@ -10,4 +10,12 @@ public class Ceiling {
         this.height = height;
         this.paintedColor = paintedColor;
     }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public PaintColor getPaintedColor() {
+        return paintedColor;
+    }
 }
