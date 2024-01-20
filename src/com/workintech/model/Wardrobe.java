b4b780237dae10c95b6ec218c81cfceb954aafe5
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 22:11:43 +0300

    constructor olusturuldu

--- src/com/workintech/model/Wardrobe.java
+++ src/com/workintech/model/Wardrobe.java
@@ -4,4 +4,10 @@ public class Wardrobe {
     private int width;
     private int height;
     private double weight;
+
+    public Wardrobe(int width, int height, double weight) {
+        this.width = width;
+        this.height = height;
+        this.weight = weight;
+    }
 }
