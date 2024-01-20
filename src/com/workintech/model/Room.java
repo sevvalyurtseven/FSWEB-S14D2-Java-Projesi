2b4b618e87ca53efd8e9344272837500fe18df43
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 23:32:28 +0300

    room isminde bir class olusturuldu ve bir odada olmasi gereken ozellikler oraya atandi

---
+++ src/com/workintech/model/Room.java
@@ -0,0 +1,21 @@
+package com.workintech.model;
+
+public class Room {
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Ceiling ceiling;
+    private Lamp lamp;
+    private Carpet carpet;
+
+    public Room(Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Lamp lamp, Carpet carpet) {
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.lamp = lamp;
+        this.carpet = carpet;
+    }
+}
