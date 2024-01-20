42c7780b4ace8354e49f72c3d631ae55ac62a22a
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 22:27:29 +0300

    constructor olusturuldu

--- src/com/workintech/model/Carpet.java
+++ src/com/workintech/model/Carpet.java
@@ -6,4 +6,10 @@ public class Carpet {
     private int width;
     private int height;
     private PaintColor color;
+
+    public Carpet(int width, int height, PaintColor color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
 }
