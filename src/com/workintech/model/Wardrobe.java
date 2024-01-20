3a5b95b0af208712ab1cab7fa5e8482b51415412
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 22:13:38 +0300

    toString eklendi

--- src/com/workintech/model/Wardrobe.java
+++ src/com/workintech/model/Wardrobe.java
@@ -26,4 +26,13 @@ public class Wardrobe {
     public void add(){
         System.out.println("Wardrobe added into Bedroom.");
     }
+
+    @Override
+    public String toString() {
+        return "Wardrobe{" +
+                "width=" + width +
+                ", height=" + height +
+                ", weight=" + weight +
+                '}';
+    }
 }
