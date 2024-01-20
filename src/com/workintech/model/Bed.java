ae698625fbe4d435469cd06bce703fea4310a780
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 22:07:00 +0300

    getter methodlari olusturuldu

--- src/com/workintech/model/Bed.java
+++ src/com/workintech/model/Bed.java
@@ -5,13 +5,29 @@ public class Bed {
     private int pillows;
     private int height;
     private int sheets;
-    private int quilt;
+    private int quilts;
 
-    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+    public Bed(String style, int pillows, int height, int sheets, int quilts) {
         this.style = style;
         this.pillows = pillows;
         this.height = height;
         this.sheets = sheets;
-        this.quilt = quilt;
+        this.quilts = quilts;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilts() {
+        return quilts;
     }
 }
