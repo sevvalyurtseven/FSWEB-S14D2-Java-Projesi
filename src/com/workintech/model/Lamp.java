3118dd2e97745f2752a00262525f8c0dcb40c56e
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-20 21:52:53 +0300

    lamp class'i icin instance variable'lar tanimlandi

---
+++ src/com/workintech/model/Lamp.java
@@ -0,0 +1,9 @@
+package com.workintech.model;
+
+import com.workintech.enums.LampType;
+
+public class Lamp {
+    private LampType style;
+    private boolean battery;
+    private int globRating;
+}
